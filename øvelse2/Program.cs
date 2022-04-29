3be3cbf81955ce0d4b783159540a5eb446696322
Real-vokS <danielcskov@gmail.com>
2022-04-29 10:44:10 +0200

    added comments

--- øvelse2/Program.cs
+++ øvelse2/Program.cs
@@ -10,9 +10,11 @@ namespace øvelse2
         static Thread t2 = new Thread(SayMore);
         static void Main(string[] args)
         {
+            //giver t1 et navn og starter den
             t1.Name = "Thread1";
             t1.Start();
 
+            //giver t2 et navn og starter den
             t2.Name = "Thread2";
             t2.Start();
 
@@ -22,18 +24,22 @@ namespace øvelse2
 
         static void SayEasy()
         {
+            //udskriver en string i Console.WriteLine og timeouter tråden i 1 sek før den fortsætter
             for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine("[{0}] C# trådning er nemt", Thread.CurrentThread.Name);
+                //timeouter Tråden i 1 sek
                 Thread.CurrentThread.Join(1000);
             }
         }
 
         static void SayMore()
         {
+            //udskriver en string i Console.WriteLine og timeouter tråden i 1 sek før den fortsætter
             for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine("[{0}] Også med flere tråde..", Thread.CurrentThread.Name);
+                //timeouter Tråden i 1 sek
                 Thread.CurrentThread.Join(1000);
             }
         }
